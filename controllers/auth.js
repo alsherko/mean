c7cd5cf564b02be5ab988d49bbd4e3c9acb20f8a
alsherko <al.sherko@gmail.com>
2020-12-12 23:48:09 +0300

    add registration route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+
 module.exports.login = (req, res) => {
     res.status(200).json({
         login: {
@@ -7,6 +10,27 @@ module.exports.login = (req, res) => {
     })
 }
 
-module.exports.register = (req, res) => {
+module.exports.register = async (req, res) => {
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        res.status(409).json({
+            massage: 'Такой email уже занят. Попробуйте другой'
+        })
+    } else {
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch (e) {
+
+        }
+    }
 
 }
