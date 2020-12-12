640e720187afae39bd6a0951d6a3b1d61828bde6
alsherko <al.sherko@gmail.com>
2020-12-13 00:25:46 +0300

    add passport

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
@@ -16,6 +17,9 @@ const app = express()
 mongoose.connect(keys.mongoURL)
     .then(() => console.log('Mongo connected'))
     .catch(error => console.log(error))
+
+app.use(passport.initialize())
+require('./middleware/passtort')(passport)
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
